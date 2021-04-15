3ecca0d4bfde72b4b34abaa2c25f2f61a0c5b7a8
krismandev <krismanpratama@gmail.com>
2021-04-15 11:10:40 +0700

    bg

--- resources/views/kdtujuan.blade.php
+++ resources/views/kdtujuan.blade.php
@@ -6,19 +6,7 @@
 ?>
 
 @section('content')
-<style>
-    .card{
-      backgroud-image: url('{{asset("images/background_new1.jpeg")}}') !important;
-    }
 
-    .card-header{
-      backgroud-image: url('{{asset("images/background_new1.jpeg")}}') !important;
-    }
-
-    .card-footer{
-      backgroud-image: url('{{asset("images/background_new1.jpeg")}}') !important;
-    }
-</style>
 @if(auth()->user()->role==2)
 <!-- Main content -->
  <div class="main-content" id="panel">
